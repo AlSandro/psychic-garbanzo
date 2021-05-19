e86335f0348931e7a05941da4aeab6547413003c
Serge-boro <serhiiborodin83@gmail.com>
2021-05-19 16:51:57 -0700

    we made modal window in the project

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ const open =document.querySelector('#open'),
       close.addEventListener('click', ()=> {
       	container.classList.remove('show-nav')
       })
+      
+
+// modal window
+ const modalClose = document.querySelector('.modal-close'),
+      modalOpen = document.querySelector('.open-modal'),
+      modal = document.querySelector('.modal')
+
+
+      modalOpen.addEventListener('click', ()=> {
+        modal.classList.add('show')
+        container.classList.add('blur')
+        document.body.style.overflow = 'hidden'
+      })
+
+      closeModal = () => {
+        modal.classList.remove('show')
+        container.classList.remove('blur')
+        document.body.style.overflow = ''
+      }
+
+      modal.addEventListener('click', (e)=> {
+        if (e.target === modal || e.target.classList.contains('modal-close')){
+            closeModal()
+        }
+      })
